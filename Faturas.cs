3e357aa2f914ce722905525fd6c6137bf9604da6
Rey-Leal <reinaldoleal2@gmail.com>
2023-11-02 16:28:23 -0300

    Leitura Inicial

---
+++ Faturas.cs
@@ -0,0 +1,48 @@
+﻿
+public class Faturas
+{
+    public bool ProcessarFaturas(string arquivoDeFaturas)
+    {
+        try
+        {
+            string linha;
+            bool existeArquivo = File.Exists(arquivoDeFaturas);
+
+            if (existeArquivo)
+            {
+                using (StreamReader reader = new StreamReader(arquivoDeFaturas))
+                {
+                    while ((linha = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+                // Processou arquivo com sucesso
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Arquivo não existe!\nInforme o caminho correto do arquivo de faturas!");
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Erro na leitura da fatura:\n" + e.Message);
+            return false;
+        }
+    }
+
+    public int Somar(int a, int b)
+    {
+        try
+        {
+            return a + b;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Erro:\n" + e.Message);
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
 ﻿// Teste Hiperstream - Analista de Sistemas Pleno
-Console.WriteLine("Teste Hiperstream!");
+using System;
+using System.IO;
+
+namespace hiperstream;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            bool repetirExecucao = true;
+
+            while (repetirExecucao)
+            {
+                Console.WriteLine("Olá seja bem vindo ao Teste Hiperstream!");
+
+                //string arquivoDeFaturas = "E:\\Rey Leal\\Desktop\\Hiperstream\\base_hi.txt";
+
+                Console.WriteLine("Digite o caminho do diretório aonde se encontra o arquivo 'base_hi.txt':");
+                string arquivoDeFaturas = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(arquivoDeFaturas))
+                {
+                    string extensao = arquivoDeFaturas.Substring(arquivoDeFaturas.Length - 4);
+                    if (extensao == ".txt")
+                    {
+                        Faturas faturas = new Faturas();
+                        faturas.ProcessarFaturas(arquivoDeFaturas);
+                    }
+                    else
+                    {
+                        Console.WriteLine("O sistema aceita apenas arquivos de faturas em formato .txt!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Obrigatório informar o caminho do diretório!");
+                }
+
+                Console.WriteLine("Para repetir a execução digite 'sim' ou qualquer outra tecla para sair:");
+                string entrada = Console.ReadLine();
+
+                repetirExecucao = entrada.ToLower() == "sim" ? true : false;
+                Console.Clear();
+            }            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Erro na leitura da fatura:\n" + e.Message);
+        }
+    }
+}
